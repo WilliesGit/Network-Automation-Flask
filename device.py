42a063ffd84c9ba2995a80840c811e8e17ed80b2
UDUAK AKPADOK <WilliesGit>
2025-11-24 13:31:07 +0000

    First commit

---
+++ device.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+  return render_template("dashboard.html")
+
+
+@app.route("/hostname", methods=["GET", "POST"])
+def hostname():
+  return render_template("hostname.html")
+
+
+@app.route("/runningConfig", methods=["GET", "POST"])
+def runningConfig():
+  return render_template("running-config.html")
+
+
+@app.route("/startupConfig", methods=["GET", "POST"])
+def startupConfig():
+  return render_template("startup-config.html")
+
+
+@app.route("/compareConfigs", methods=["GET", "POST"])
+def compareConfigs():
+  return render_template("compare-config.html")
+
+
+@app.route("/ciscoHardening", methods=["GET", "POST"])
+def ciscoHardening():
+  return render_template("cisco-hardening.html")
+
+
+@app.route("/loopback", methods=["GET", "POST"])
+def loopback():
+  return render_template("loopback.html")
+
+
+@app.route("/routeProtocols", methods=["GET", "POST"])
+def routeProtocols():
+  return render_template("route-protocols.html")
+
+@app.route("/ACL", methods=["GET", "POST"])
+def ACL():
+  return render_template("ACL.html")
+
+
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
