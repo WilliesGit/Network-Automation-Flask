360ba46fae39a718c0639b387a73b6c29310c333
UDUAK AKPADOK <WilliesGit>
2026-02-04 12:58:13 +0000

    Updated device.py

--- device.py
+++ device.py
@@ -1265,5 +1265,59 @@ def route_protocol():
   return jsonify({'results': results}), status_code
 
 
+#API endpoint to configure ACLs on devices
+@app.route("/api/acl_config", methods=['POST'])
+def acl_config():
+
+  #Retrieve user input as JSON data from the request
+  data = request.json
+
+  #List to store result to be sent back for processing
+  results = []
+  threads = []
+  lock = threading.Lock() 
+
+ 
+  devices = data['devices']
+  acl_config = data['acl_config']
+  dir_config = data['dir_config']
+
+
+  def worker(device_key, device_info):
+      result = aclConfig(device_key, acl_config, dir_config, devices_db)
+      with lock:
+          results.append(result)
+
+  #Create a thread per device
+  for device_key, device_info in devices.items():
+    print('Device keys: ',device_key)
+
+    t = threading.Thread(target=worker, args=(device_key, device_info))
+    threads.append(t)
+    t.start()
+
+
+  #Wait for all threads to complete
+  for t in threads:
+      t.join()
+    
+
+  any_success = False
+  for r in results:
+      if r.get('status') == 'success':
+          any_success = True
+          break 
+      
+
+  if any_success:
+      status_code = 200 
+
+  else:
+      status_code = 400
+
+  return jsonify({'results': results}), status_code
+
+
+
 if __name__ == "__main__":
   app.run(debug=True)
